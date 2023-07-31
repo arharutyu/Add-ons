625d666d0eae89a908cc7543edae515fd9d0c7b9
arharuty <13937@coderacademy.edu.au>
2023-07-31 16:16:40 +1000

    refactor useState for levels

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import { useState } from 'react'
 import './App.css'
-import GetSkill from './GetSkill'
 import SkillList from './SkillList'
 import ChooseLevel from './ChooseLevel'
 
 function App() {
-  const [skill, setSkill] = useState(0)
+  const [level, setLevel] = useState("Level 1")
   
   return (
     <>
       <h1>Add Ons Cheer Challenge</h1>
-      <ChooseLevel />
-      <SkillList />
+      <ChooseLevel level={level} setLevel={setLevel}/>
+      <SkillList level={level} />
 
     </>
   )
