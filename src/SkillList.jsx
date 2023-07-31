1bdc5ffb2d26034b40990c8903b555dfd503bf97
arharuty <13937@coderacademy.edu.au>
2023-07-31 16:17:41 +1000

    get skill list from levels in state refactor

--- src/SkillList.jsx
+++ src/SkillList.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
-import GetSkill from './GetSkill'
 import ClearSkillList from './ClearSkillList'
+import levels from './AllSkills.json'
 
+const SkillList = ({ level }) => {
+  function GetSkill(level) {
+    let level_skills = levels[level]
+    
+    // Create array of skills object keys
+    const keys = Object.keys(level_skills)
+  
+    // Generate random index based on number of keys
+    const randIndex = Math.floor(Math.random() * keys.length)
+  
+    // Select a key from the array of keys using the random index
+    const randKey = keys[randIndex]
+  
+    // Use the key to get the corresponding name from the "names" object
+    const newSkill = level_skills[randKey]
+    return newSkill
+  }
 
-const SkillList = (props) => {
   return (
     <div>Skill List
         <ol></ol>
-        <button onClick={GetSkill}>Add skill</button>
+        <button onClick={() => document.querySelector('ol').innerHTML += `<li>${GetSkill(level)}</li>`}>Add skill</button>
         <button onClick={ClearSkillList}>Reset</button>
     </div>
   )
